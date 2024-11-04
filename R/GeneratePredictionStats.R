9a4408595be1151badb27d9c233b541ae91667e2
tonyxieuoft <xiaohan.xie@mail.utoronto.ca>
2024-11-04 01:08:34 -0500

    Formatting changes, making more use of existing packages and functions, document the parsing function

--- R/GeneratePredictionStats.R
+++ R/GeneratePredictionStats.R
@@ -1,6 +1,22 @@
 # and then also visualize output, no?
 
 
+#'
+#' Create Heatmap
+#'
+#' filler description
+#'
+#' @param seq_gaps_melted Melted dataframe of sequence coverage
+#'
+#' @import ggplot2
+create_seq_gap_heatmap <- function(seq_gaps_melted){
+  ggplot2::ggplot(seq_gaps_melted, aes(x = species_name,
+                                       y = gene_name,
+                                       fill = coverage)) +
+    geom_tile()
+}
+
+
 #' HeatMap Generator Helper
 #'
 #' Filler Description
@@ -8,23 +24,44 @@
 #' @param prediction_df Dataframe created by Build Predictions
 get_seq_gaps <- function(prediction_df){
 
-  gene_names <- get_unique_genes(prediction_df)
+  total_gene_names <- get_unique_genes(prediction_df)
+  print(total_gene_names)
   species_names <- names(prediction_df)
-  L <- length(gene_names)*length(species_names)
+  L <- length(total_gene_names)*length(species_names)
+  print(L)
 
   seq_gaps_melted <- data.frame(species_name = character(length = L),
-                                gene_names = character(length = L),
-                                gap_percent = numeric(length = L))
+                                gene_name = character(length = L),
+                                coverage = numeric(length = L))
 
+  curr_index <- 1
 
   for (i in seq_along(prediction_df)){
 
     gene_names <- names(prediction_df[[i]])
-    print(gene_names)
-    for (seq in prediction_df[[i]]){
-      print(seq)
+
+    for (j in seq_along(prediction_df[[i]])){
+      seq_gaps_melted$species_name[curr_index] <- species_names[i]
+      seq_gaps_melted$gene_name[curr_index] <- gene_names[j]
+      seq_gaps_melted$coverage[curr_index] <-
+        get_coverage(prediction_df[[i]][[j]])
+
+
+
+      curr_index = curr_index + 1
+    }
+
+    non_included_genes <- dplyr::setdiff(gene_names, total_gene_names)
+    for (gene in non_included_genes){
+
+      seq_gaps_melted$species_name[curr_index] <- species_names[i]
+      seq_gaps_melted$gene_name[curr_index] <- gene_names[j]
+      seq_gaps_melted$coverage[curr_index] <- NA
+      curr_index = curr_index + 1
     }
   }
+
+  return(seq_gaps_melted)
 }
 
 
@@ -51,7 +88,7 @@ get_coverage <- function(seq){
 get_unique_genes <- function(prediction_df){
 
   total_gene_list = new.env()
-  for (genes in seq_along(prediction_df)){
+  for (genes in prediction_df){
     curr_gene_list <- names(genes)
     for (gene in curr_gene_list){
       if (is.null(total_gene_list[[gene]])){
--- R/ParseInput.R
+++ R/ParseInput.R
@@ -1,20 +1,82 @@
 
-#' Parse Input Title
+#' Parse JSON-format BLAST output data
 #'
-#' Parse Input Description
+#' This function facilitates the conversion of a BLAST output file in .json
+#' format (in which the quer(ies) are coding sequences and the set of subject
+#' genomic sequences is from ONE species only) into a nested R list data
+#' structure. The
+#' raw data in this format serves as input for the build_predictions
+#' functions. Assumes that the first word in the fasta heading(s) of the query
+#' coding sequence(s) is the abbreviated gene name for the query.
 #'
-#' @param filename File name of the BLAST XML/json input file to parse
-#' @param species Species from which the subject genome is from.
-#' @param raw_carrier
+#' @param filename File path of a single BLAST output file in .json format.
+#' Output must be obtained in the context of input query coding sequences and a
+#' set of genomic sequences from a SINGLE species.
+#' @param species Name of species (string) represented by the set of subject
+#' genomic sequences blasted against. Note that for the ease of the user, gene
+#' names are extracted from query fasta headings rather than provided as a direct argument
+#' @param raw_list Nested R list previously created by the
+#' package, contains raw BLAST output data. Fill in only if you wish to group
+#' two sets of raw data together for visualization. THe default value is NULL.
+#'
+#' @return A nested R list data structure storing the raw BLAST output, as
+#' follows:
+#' \itemize{
+#'  \item The outer list is in key-value format where each key is a
+#'  species name and the corresponding value is a "gene list" for the species
+#'  \item Each "gene list" is also in key-value format, where each key is a
+#'  gene name and the  corresponding value is a list containing two attributes:
+#'  1) \emph{hsp_data}, a data frame of high-scoring pairs identified by BLAST
+#'  between the query coding sequence for the gene and subject genomic sequences
+#'  and 2) \emph{query_len}, the length of the query coding sequence for the
+#'  gene.
+#'  \item Each row in \emph{hsp_data} corresponds to a single high-scoring pair,
+#'  where the columns are as follows:
+#'  \itemize{
+#'    \item \emph{q_start}: start of the HSP segment on the query side.
+#'    \item \emph{q_end}: end of the HSP segment on the query side.
+#'    \item \emph{q_seq}: nucleotide sequence of the HSP segment on the query
+#'    side
+#'    \item \emph{s_start}, \emph{s_end} and \emph{s_seq} are the same, but on
+#'    the subject side.
+#'    \item \emph{s_strand}: The strand of the subject segment (Plus/Minus)
+#'    \item \emph{e_val}: The HSP's e-value
+#'    \item \emph{seq_len}: Length of the HSP
+#'  }
+#' }
+#' The list is a concatenation of raw_list with the raw BLAST output pointed to
+#' by filename.
+#'
+#'
 #'
 #' @import rjson
 #' @import dplyr
 #' @import readr
 #' @import stringr
+#' @import configr
 #'
 #' @export
+#'
+#' @examples
+#' json_file1 <- system.file("extdata", "orca_rhfdsafo_BLAST_output.json",
+#'                            package="euPredictR")
+#' # Example 1: No raw_list input argument
+#'
+#' raw_list1 <- parse_BLAST_json(filename = json_file1, species = "O_orca")
+#'
+#' # Example 2: Using raw list generated previously as additional input
+#'
+#' raw_list2 <- parse_BLAST_json(filename = json_file1, species = "T_truncatus",
+#'                               raw_list = raw_list1)
+#' # this list concatenates raw BLAST results for both orca and dolphin RHO into
+#' # one list
 
-parse_BLAST_json <- function(filename, species, raw_carrier=NULL){
+parse_BLAST_json <- function(filename, species, raw_list=NULL){
+
+  if (! configr::is.json.file(filename)) {
+    stop("Inputted file is not in JSON format.")
+  }
+  else {} # Continue
 
   # reads the json file, removes all newlines, then stores in list format
   json_data <- readr::read_file(filename) %>%
@@ -56,56 +118,39 @@ parse_BLAST_json <- function(filename, species, raw_carrier=NULL){
       hsps <- top_hit$hsps
       L <- length(hsps)
 
-      hsp_data <- data.frame(q_start = numeric(length = L),
-                             q_end = numeric(length = L),
-                             q_seq = character(length = L),
-                             s_start = numeric(length = L),
-                             s_end = numeric(length = L),
-                             s_seq = character(length = L),
-                             s_strand = character(length = L),
-                             e_val = numeric(length = L),
-                             seq_len = numeric(length = L))
+      hsp_data <- do.call(rbind, lapply(hsps, data.frame)) %>%
+        subset(select = -c(num, bit_score, score, identity, query_strand,
+                           align_len, midline, gaps))
 
-      for (hsp in top_hit$hsps){
+      colnames(hsp_data) <- c("e_val", "q_start", "q_end", "s_start", "s_end",
+                              "s_strand", "q_seq", "s_seq")
 
-        i <- hsp$num
+      hsp_data <- dplyr::mutate(hsp_data, seq_len = q_end - q_start + 1) %>%
+        remove_gaps_if_equal()
 
-        hsp_data$q_start[i] <- hsp$query_from
-        hsp_data$q_end[i] <- hsp$query_to
-
-        hsp_data$s_start[i] <- hsp$hit_from
-        hsp_data$s_end[i] <- hsp$hit_to
-
-        gaps_q_seq <- stringr::str_count(hsp$qseq, "-")
-        gaps_s_seq <- stringr::str_count(hsp$hseq, "-")
-        if (gaps_q_seq == gaps_s_seq & gaps_q_seq > 0){
-          hsp_data$q_seq[i] <- gsub('-', '', hsp$qseq)
-          hsp_data$s_seq[i] <- gsub('-', '', hsp$hseq)
-        }
-        else{
-          hsp_data$q_seq[i] <- hsp$qseq
-          hsp_data$s_seq[i] <- hsp$hseq
-        }
-
-        hsp_data$e_val[i] <- hsp$evalue
-        hsp_data$s_strand[i] <- hsp$hit_strand
-
-        hsp_data$seq_len[i] <- (hsp$query_to - hsp$query_from + 1)
-      }
+      print(hsp_data)
       genes[[query_name]] <- list(query_len = query_len, hsp_data = hsp_data)
     }
   }
 
-  if (!is.null(raw_carrier)){
-    raw_carrier[[species]] <- genes
+  if (!is.null(raw_list)){
+    raw_list[[species]] <- genes
   }
   else{
-    raw_carrier <- list()
-    raw_carrier[[species]] <- genes
+    raw_list <- list()
+    raw_list[[species]] <- genes
   }
 
-  return(raw_carrier)
+  return(raw_list)
 }
+
+#' Title
+#'
+#' Description
+validate_BLAST_JSON_file <- function(){
+  print(1)
+}
+
 
 
 #' Parse Multiple BLAST json
@@ -114,21 +159,46 @@ parse_BLAST_json <- function(filename, species, raw_carrier=NULL){
 #'
 #' @param dir_path path to directory containing json files from different
 #' species
-#' @param raw_carrier previous raw carrier, if available
+#' @param raw_list previous raw carrier, if available
 #'
 #' @import tools
 
-parse_multiple_BLAST_json <- function(dir_path, raw_carrier=NULL){
+parse_multiple_BLAST_json <- function(dir_path, raw_list=NULL){
 
   all_files <- list.files(path = dir_path, full.names = TRUE)
 
-  raw_carrier <- NULL
+  raw_list <- NULL
   for (filename in all_files){
     species_name <- tools::file_path_sans_ext(basename(filename))
-    raw_carrier <- parse_BLAST_json(filename, species_name, raw_carrier)
+    raw_list <- parse_BLAST_json(filename, species_name, raw_list)
   }
-  return(raw_carrier)
+  return(raw_list)
 }
+
+
+#' Check if number of gaps is equal and remove if so
+#'
+#' Dummy Description
+#'
+#' @param hsp_data Table containing HSP data
+#'
+#' @import stringr
+#'
+remove_gaps_if_equal <- function(hsp_data){
+
+  for (i in seq_along(hsp_data$q_seq)){
+    gaps_q_seq <- stringr::str_count(hsp_data$q_seq[i], "-")
+    gaps_s_seq <- stringr::str_count(hsp_data$s_seq[i], "-")
+
+    if (gaps_q_seq == gaps_s_seq & gaps_q_seq > 0){
+      hsp_data$q_seq[i] <- gsub('-', '', hsp_data$q_seq[i])
+      hsp_data$s_seq[i] <- gsub('-', '', hsp_data$s_seq[i])
+    }
+  }
+  return(hsp_data)
+}
+
+
 
 
 
